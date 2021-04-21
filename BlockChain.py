b1015a5a5bd6b09af4967ca8b5ca373ce1a11a47
cn <cn0806@naver.com>
2021-04-21 15:11:57 +0900

    블록체인 완료

--- BlockChain.py
+++ BlockChain.py
@@ -1,14 +1,76 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
+from uuid import uuid4
 
-class Blockchain(object):
+import requests
+from flask import Flask, jsonify, request
+
+
+
+class Blockchain:
     def __init__(self):
         self.current_transactions=[]
         self.chain=[]
+        self.nodes = set()
        
        self.new_block(previous_hash=1, proof=100)
     
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
+
+    def valid_chain(self, chain):
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n------------\n")
+            last_block_hash = self.hash(last_block)
+            if block['previous_hash'] != last_block_hash:
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return False    
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        
+        max_length = len(self.chain)
+        
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+            
+        if new_chain:
+            self.chain = new_chain
+            return True
+        
+        return False
+
     def new_block(self, proof, previous_hash=None):
         block = {
             'index':len(self.chain)+1,
@@ -22,14 +84,15 @@ class Blockchain(object):
         self.chain.append(block)
         return block
         
-        def new_transaction(self, sender, recipient, amount):
-            self.current_transactions.append({
-                'sender': sender,
-                'recipient': recipient,
-                'amount': amount,
-            })
+    def new_transaction(self, sender, recipient, amount):
+        
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
 
-            return self.last_block['index']+1
+        return self.last_block['index']+1
     
     @property
     def last_block(self):
@@ -39,3 +102,112 @@ class Blockchain(object):
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_block):
+        last_proof = last_block['proof']
+        last_hash = self.hash(last_block)
+
+        proof = 0
+        while self.valid_proof(last_proof,  proof, last_hash) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof, last_hash):
+        guess = f'{last_proof}{proof}{last_hash}' .encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = blockchain()
+
+@app.route('/mine', methods=['get'])
+def mine():
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Blcok Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof']
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['post'])
+def new_transaction():
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonfy(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['post'])
+def register_nodes():
+    values  = request.get_json()
+
+    nodes = values.get('nodes')
+    if node is None:
+        return "Error: Please supply a valid lsit of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+    
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+@app.route('nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+if __name__=='__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
